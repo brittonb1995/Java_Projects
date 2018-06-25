a8b39dea0ef0dd05f18735075c70b6a7156b0eee
Britton Beeny <brittonbeeny@Brittons-MacBook-Pro.local>
2018-06-25 18:26:26 -0500

    Start of a webscraping weather app

---
+++ Java_WeatherApp/src/Java_WeatherApp.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+public class Java_WeatherApp {
+
+	public static void main(String[] args) {
+		
+		String url = "https://weather.com/weather/today/l/USTX0461:1:US";
+	}
+
+}
